5544c04b7b40066662860937520b44c9330dfa21
Richard Hildred <rhildred@capsoln.ca>
2013-09-01 03:29:56 -0400

    changed server.js to use res.sendFile

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
+var ejs = require('ejs-locals');
 var fs = require('fs');
-var mime = require('mime');
-var engine = require('ejs-locals');
+var path = require('path');
 
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || parseInt(process.argv.pop());
@@ -13,35 +13,40 @@ app.configure(function() {
 });
 
 app.use(function(req, res) {
-	var sUrl = "/index.js.html";
-	if (req.url != "/")
-		sUrl = req.url;
-	if (-1 == sUrl.search("js.html")) {
-		fs.readFile("views/" + sUrl, function(err, data) {
-			if (err) {
-				res.writeHead(404);
-				return res.end("File not found.");
-			}
-
-			res.setHeader("Content-Type", mime.lookup(sUrl)); // Solution!
-			res.writeHead(200);
-			res.end(data);
-		});
+	var sUrl = "views/index.js.html";
+	if (req._parsedUrl.pathname == "/") {
+		if (!fs.existsSync(sUrl)) {
+			sUrl = "views/index.html";
+		}
 	} else {
-		var sOut = "";
-		var options = {
-				settings:{
-					'view engine': 'js.html'
+		sUrl = "views" + req._parsedUrl.pathname;
+	}
+	if (path.normalize(sUrl).search("views") == -1) {
+		// we don't seem to be able to get a path below views
+		res.send(403, 'Unauthorized.');
+	} else if (fs.existsSync(sUrl)) {
+		// serve file
+		if (-1 == sUrl.search("js.html")) {
+			// then it is sent without parsing
+			res.sendfile(sUrl);
+		} else {
+			// then we want to parse this file as js.html with ejs
+			var options = {
+				settings : {
+					'view engine' : 'js.html'
 				}
-		};
-		engine("views" + sUrl, options, function(err, html){
-			if(err){
-				console.log(err);
-			}else{
-				res.setHeader("Content-Type", "text/html");
-				res.end(html);
-			}
-		});
+			};
+			ejs(sUrl, options, function(err, html) {
+				if (err) {
+					console.log(err);
+				} else {
+					res.setHeader("Content-Type", "text/html");
+					res.end(html);
+				}
+			});
+		}
+	} else {
+		res.send(404, 'File not Found.');
 	}
 });
 
